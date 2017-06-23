df166515bffabf2dd48125bc8ed26c8fe09d2b6d
李文强 <liwenqiang@che001.com>
2017-06-23 17:04:44 +0800

    dataAcquisition init

---
+++ README.md
@@ -0,0 +1,137 @@
+## 前端数据采集上报插件 dataAcquisition.js
+
+### 目的：
+1. 实现前端数据上报分析
+2. 实现用户画像建模，轨迹分析
+3. 实现主动埋点上报以及自动埋点
+4. 搭配node接口实现日志存储解析
+5. 基于powerBI实现数据分析展示
+6. 实现前端页面加载速度上报 
+7. 实现前端接口异常上报     
+8. 实现前端代码异常上报	
+9. 对接口进行改造，使其与后台日志关联起来		未完成
+
+### 日志：
+2017-04-03 - 实现基本页面访问数据上报
+
+2017-04-22 - 实现点击数据上报
+
+2017-05-28 - 全功能完成测试中
+
+2017-05-31 - 正式第一版部署到ult进行采集
+
+2017-06-09 - 爆出cookie过大导致400异常
+
+2017-06-14 - powerBI展示层完成
+
+2017-06-15 - 立项保存
+
+版本升级：1.0.1
+
+2017-06-18 - 增加点击元素采集限制，避免数据过大
+
+2017-06-20 - 增加输入框采集类型配置
+
+2017-06-21 - 增加接口异常上报，代理ajax error处理函数
+
+2017-06-22 - 增加代码异常上报。监听onerror事件
+
+2017-06-23 - 增加performance API统计页面加载时间信息
+
+### 配置参数：
+	 	storeVer     : '1.0.1',     //版本号
+        storeInput   : "ACINPUT",   //输入采集
+        storePage    : "ACPAGE",    //页面采集
+        storeClick   : "ACCLIK",    //点击事件采集
+        storeReqErr  : "ACRERR",    //请求异常采集
+        storeTiming  : "ACTIME",    //页面时间采集
+        storeCodeErr : "ACCERR",    //代码异常采集
+        sendUrl      : "http://localhost:9090/logStash/push",   //log采集地址
+        selector     : 'input',     //通过控制输入框的选择器来限定监听范围$("*[id^='qyd_aci']");
+        acRange      : ['text','tel','password'],   //输入框采集范围
+        userSha      : 'userSha',   //用户标识
+        maxDays      : 5,           //cookie期限
+        acblength    : 2,           //点击元素采集层数
+        useStorage   : false        //自动检测是否使用storage，不要手动更改
+### 数据格式：
+1. 行为数据
+
+		{
+			"uuid":"F6A6C801B7197603",        //用户标识，5天有效
+			"acData":[						  //数据集
+					{
+					"type":"ACINPUT/ACPAGE/ACCLIK",     //上报数据类型：输入框/页面访问/点击事件
+					"path":"www.domain.com/w/w/w/",   //事件发生的url
+					"eId":"qyd_acb_0_1",				  //事件发生的元素ID	
+					"class":"js_acb_2_0",				  //事件发生的元素class	
+					"sTme":"13000000",				  //事件发生开始时间
+					"eTme":"130020122",				  //事件结束事件							
+					"val":"123,3000:1234,4000:12345",   //事件发生后不同时间元素的值	  		
+					"utk":"usertoken"					  //关联后台日志							（未实现）
+				}
+			]
+		}
+	
+2. 接口异常数据
+	
+		{
+	        "type":"ACRERR",     //上报数据类型：接口异常
+	        "path":"www.domain.com/w/w/w/",   //事件发生页面的url
+	        "sTme":"2017-06-21 13:31:31",				  //事件发生时间
+	        "requrl"     : "/mt/klalsjdjlenm",       //接口地址
+	        "readyState" : "2",    //当前状态,0-未初始化，1-正在载入，2-已经载入，3-数据进行交互，4-完成。
+	        "status"     : "301",  //请求状态码：400，500，404
+	        "statusText" : "Internal Server Error",  //404错误信息是not found,500是Internal Server Error。
+			"textStatus" : "parsererror",  //timeout"（超时）, "error"（错误）, "abort"(中止), "parsererror"（解析错误）     
+	    }
+    
+3. 代码异常数据
+	
+		{
+	        "type":"ACCERR",     //上报数据类型：代码异常
+	        "path":"www.domain.com/w/w/w/",   //事件发生页面的url
+	        "sTme":"2017-06-21 13:31:31",	  //事件发生时间
+	        "msg"     : "script error",       //异常摘要
+	        "line"    : "301",  //代码行数
+	        "col"     : "异常",  //异常堆栈数据
+			 "err"     : "异常信息",  
+			 "ua"		 : "ios/chrome 44.44"  //浏览器版本
+	    }
+    
+4. 时间数据
+	
+		{
+		    "type":"ACTIME",     //上报数据类型：代码异常
+		    "path":"www.domain.com/w/w/w/",   //事件发生页面的url
+		    "DNS"     : "152",       //DNS查询时间
+		    "TCP"     : "525",  		//TCP连接耗时
+		    "WT"      : "555",  	   //白屏时间
+		    "DR"      : "123", 		//dom ready时间，脚本加载完成时间
+			 "ONL"		 : "152",  		//执行onload事件耗时
+			 "ALLRT"	 : "152",  		//所有请求耗时
+			 "PRDOM"	 : "152",  		//dom解析耗时
+			 "FXHR"	 : "152"  		//第一个请求发起时间
+		}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ src/dataAcquisition.js
@@ -0,0 +1,351 @@
+/**
+ * Created by Risker on 2017/4/1.
+ */
+var dataAcquisition = {
+    store:{ //配置项
+        storeVer     : '1.0.1',     //版本号
+        storeInput   : "ACINPUT",   //输入采集
+        storePage    : "ACPAGE",    //页面采集
+        storeClick   : "ACCLIK",    //点击事件采集
+        storeReqErr  : "ACRERR",    //请求异常采集
+        storeTiming  : "ACTIME",    //页面时间采集
+        storeCodeErr : "ACCERR",    //代码异常采集
+        sendUrl      : "https://172.16.20.45:80/logStash/push",   //log采集地址
+        //sendUrl    : "http://localhost:9090/logStash/push",   //log采集地址
+        selector     : 'input',     //通过控制输入框的选择器来限定监听范围$("*[id^='qyd_aci']");
+        acRange      : ['text','tel','password'],   //输入框采集范围
+        userSha      : 'userSha',   //用户标识
+        maxDays      : 5,           //cookie期限
+        acblength    : 2,           //点击元素采集层数
+        useStorage   : false        //自动检测是否使用storage，不要手动更改
+    },
+    util: { //工具函数
+        isNullOrEmpty: function (obj) {
+            return ( obj !== 0 || obj !== "0" ) && ( obj === null || obj === undefined || obj === "" || obj === "null" || typeof obj === "undefined");
+        },
+        setCookie: function (name, value, Day) {
+            if (dataAcquisition.store.useStorage) {
+                window.localStorage.setItem(name, value);
+            } else {
+                if (!Day)Day = dataAcquisition.store.maxDays;
+                var exp = new Date();
+                exp.setTime(exp.getTime() + Day * 24 * 60 * 60000);
+                document.cookie = name + "=" + encodeURIComponent(value) + ";expires=" + exp.toUTCString() + ";path=/";
+            }
+        },
+        getCookie: function (name) {
+            var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+            if (!name)return null;
+            if (dataAcquisition.store.useStorage) {
+                return window.localStorage.getItem(name);
+            } else {
+                if (arr = document.cookie.match(reg)) {
+                    return (decodeURIComponent(arr[2]));
+                } else {
+                    return null;
+                }
+            }
+        },
+        delCookie: function (name) {
+            if (dataAcquisition.store.useStorage) {
+                window.localStorage.removeItem(name);
+            } else {
+                this.setCookie(name, '', -1);
+            }
+        },
+        getUuid: function (len, radix) {//uuid长度以及进制
+            var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+            var uuid = [], i;
+            if (this.isNullOrEmpty(len)) {
+                len = 16;
+            }
+            if (this.isNullOrEmpty(radix)) {
+                radix = 16;
+            }
+            for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+
+            return uuid.join('');
+        },
+        getTimeStr: function () {
+            var date = new Date();
+            var now = date.getFullYear() + "/";
+            now += (date.getMonth() + 1) + "/";
+            now += date.getDate() + " ";
+            now += date.getHours() + ":";
+            now += date.getMinutes() + ":";
+            now += date.getSeconds() + "";
+            return now;
+        }
+    },
+    init: function () {
+        this.store.useStorage = (typeof window.localStorage != 'undefined');
+        var _this = this,
+            _domList = [],
+            _ACIDoms = $(this.store.selector),
+            storePath = window.location.pathname,
+            nowStr = this.util.getTimeStr(),
+            inputAcData = this.getAc2Type(this.store.storeInput),
+            clickAcData = this.getAc2Type(this.store.storeClick),
+            reqErrAcData = this.getAc2Type(this.store.storeReqErr),
+            timingAcData = this.getAc2Type(this.store.storeTiming),
+            codeErrAcData = this.getAc2Type(this.store.storeCodeErr);
+        this.util.setCookie(this.store.storePage, storePath);
+        if (this.util.isNullOrEmpty(this.util.getCookie(this.store.userSha))) {
+            this.util.setCookie(this.store.userSha, this.util.getUuid())
+        }
+
+        //上报上一个页面数据
+        _domList.push({'type': this.store.storePage, 'path': storePath, sTme: nowStr, eTme: nowStr});
+        if (!_this.util.isNullOrEmpty(inputAcData)) {
+            for (var key in inputAcData) {
+                _domList.push(inputAcData[key]);
+            }
+        }
+        _domList = _domList.concat(clickAcData, reqErrAcData, codeErrAcData, timingAcData);
+
+        this.postData(_domList);
+
+        //对ajax绑定error处理，将异常信息做上报
+        this.bindAjaxHook();
+        //对代码异常做监控，对异常上报
+        this.bindCodeHook();
+        //对页面加载信息进行监听上报
+        this.setPerformanceAc();
+        //对本页面添加监听（ios兼容性问题）
+        if (/iphone|ipad|ipod/i.test(window.navigator.userAgent)) {
+            var elements = document.getElementsByTagName("body")[0].childNodes;
+            for (var z = 0, length = elements.length; z < length; z++) {
+                elements[z].addEventListener("click", function () {
+                });
+            }
+        }
+        //输入框事件监听
+        for (var i = 0; i < _ACIDoms.length; i++) {
+            var selector = _ACIDoms[i];
+            if (selector.type && dataAcquisition.store.acRange.indexOf(selector.type.toLowerCase()) > -1) {
+                selector.addEventListener("input", function () {
+                    dataAcquisition.setInputAc(this);
+                });
+                selector.addEventListener("focus", function () {
+                    dataAcquisition.setInputAc(this);
+                });
+                selector.addEventListener("blur", function () {
+                    dataAcquisition.setInputAc(this);
+                });
+            }
+        }
+        //点击事件监听
+        document.addEventListener("click", function (e) {
+            var event = window.event || e;
+            var target = event.srcElement ? event.srcElement : event.target;
+            _this.getACBtarget(target);
+        });
+    },
+    bindAjaxHook: function () {//对ajax中的异常进行捕获,需将代码置于业务代码之前，对所有ajax进行代理
+        var _ajax = $.ajax;
+        $.ajax = function (opts) {
+            var errorCallback = opts.error;
+            opts.error = function (XMLHttpRequest, textStatus, errorThrown) {
+                dataAcquisition.setAjErrAc(opts.url, XMLHttpRequest, textStatus);
+                errorCallback(XMLHttpRequest, textStatus, errorThrown);
+            };
+            _ajax(opts);
+        }
+    },
+    bindCodeHook: function () {
+        var _this = this;
+        window.onerror = function (msg, url, line, col, err) {
+            if (_this.util.isNullOrEmpty(url)) {
+                return true;
+            }
+            col = col || (window.event && window.event.errorCharacter) || 0;
+            var codeErrData = {
+                type: _this.store.storeCodeErr,
+                path: _this.util.getCookie(_this.store.storePage),
+                sTme: _this.util.getTimeStr(),
+                msg: msg,
+                ua: navigator.userAgent,
+                line: line,
+                col: col
+            };
+            if (!!err && !!err.stack) {
+                //可以直接使用堆栈信息
+                codeErrData.err = err.stack.toString();
+            } else if (!!arguments.callee) {
+                //尝试通过callee获取异常堆栈
+                var errmsg = [];
+                var f = arguments.callee.caller, c = 3;//防止堆栈信息过大
+                while (f && (--c > 0)) {
+                    errmsg.push(f.toString());
+                    if (f === f.caller) {
+                        break;
+                    }
+                    f = f.caller;
+                }
+                errmsg = errmsg.join(",");
+                codeErrData.err = errmsg;
+            } else {
+                codeErrData.err = "";
+            }
+            dataAcquisition.setCodeErrAc(codeErrData);
+            return true;
+        }
+    },
+    setPerformanceAc: function () {
+        var _this = this;
+        if (!!window.performance) {
+            var _PerforMance = window.performance;
+            var _Timing = _PerforMance.timing;
+            var ACPdata = [];
+            if (_Timing) {
+                var loadAcData = {
+                    type: _this.store.storeTiming,
+                    path: _this.util.getCookie(_this.store.storePage)
+                    //connectEnd : _Timing.connectEnd,     //返回浏览器与服务器之间的连接建立时的Unix毫秒时间戳
+                    //connectStart : _Timing.connectStart, //返回HTTP请求开始向服务器发送时的Unix毫秒时间戳
+                    //domComplete:_Timing.domComplete,//返回当前文档解析完成时的Unix毫秒时间戳。
+                    //domContentLoadedEventEnd : _Timing.domContentLoadedEventEnd,//返回当所有需要立即执行的脚本已经被执行（不论执行顺序）时的Unix毫秒时间戳。
+                    //domContentLoadedEventStart : _Timing.domContentLoadedEventStart,//返回当解析器发送DOMContentLoaded 事件，即所有需要被执行的脚本已经被解析时的Unix毫秒时间戳。
+                    //domInteractive : _Timing.domInteractive,//返回当前网页DOM结构结束解析、开始加载内嵌资源时的Unix毫秒时间戳。
+                    //domLoading : _Timing.domLoading,//返回当前网页DOM结构开始解析的Unix毫秒时间戳。
+                    //domainLookupEnd: _Timing.domainLookupEnd, //表征了域名查询结束的UNIX时间戳。
+                    //domainLookupStart: _Timing.domainLookupStart, //表征了域名查询开始的UNIX时间戳。
+                    //fetchStart: _Timing.fetchStart, //表征了浏览器准备好使用HTTP请求来获取(fetch)文档的UNIX时间戳。这个时间点会在检查任何应用缓存之前。
+                    //loadEventEnd: _Timing.loadEventEnd,//返回当load事件结束，即加载事件完成时的Unix毫秒时间戳。
+                    //loadEventStart: _Timing.loadEventStart,//load事件被发送时的Unix毫秒时间戳。
+                    //navigationStart: _Timing.navigationStart,//准备加载新页面的起始时间
+                    //redirectEnd: _Timing.redirectEnd,  //表征了最后一个HTTP重定向完成时（也就是说是HTTP响应的最后一个比特直接被收到的时间）的UNIX时间戳
+                    //redirectStart: _Timing.redirectStart, //表征了第一个HTTP重定向开始时的UNIX时间戳
+                    //requestStart: _Timing.requestStart,//返回浏览器向服务器发出HTTP请求时（或开始读取本地缓存时）的Unix毫秒时间戳。
+                    //responseEnd: _Timing.responseEnd,//返回浏览器从服务器收到（或从本地缓存读取，或从本地资源读取）最后一个字节时的Unix毫秒时间戳。
+                    //responseStart: _Timing.responseStart,//返回浏览器从服务器收到（或从本地缓存读取）第一个字节时的Unix毫秒时间戳
+                    //secureConnectionStart: _Timing.secureConnectionStart,  //浏览器与服务器开始安全链接的握手时的Unix毫秒时间戳
+                    //unloadEventEnd: _Timing.unloadEventEnd,    //表征了unload事件处理完成时的UNIX时间戳
+                    //unloadEventStart: _Timing.unloadEventStart //表征了unload事件抛出时的UNIX时间戳
+                };
+                loadAcData.DNS = _Timing.domainLookupEnd - _Timing.domainLookupStart; //DNS查询时间
+                loadAcData.TCP = _Timing.connectEnd - _Timing.connectStart; //TCP连接耗时
+                loadAcData.WT = _Timing.responseStart - _Timing.navigationStart; //白屏时间
+                loadAcData.DR = _Timing.domContentLoadedEventEnd - _Timing.navigationStart; //dom ready时间，脚本加载完成时间
+                loadAcData.ONL = _Timing.loadEventEnd - _Timing.navigationStart; //执行onload事件耗时
+                loadAcData.ALLRT = _Timing.responseEnd - _Timing.redirectStart; //所有请求耗时
+                loadAcData.PRDOM = _Timing.domComplete - _Timing.domInteractive; //dom解析耗时
+                loadAcData.FXHR = _Timing.fetchStart - _Timing.navigationStart; //第一个请求发起时间
+                ACPdata.push(loadAcData);
+                this.util.setCookie(this.store.storeTiming, JSON.stringify(ACPdata));
+            }
+        }
+    },
+    setCodeErrAc: function (data) {
+        var storeString = this.util.getCookie(this.store.storeCodeErr);
+        var ACCEdata = this.util.isNullOrEmpty(storeString) ? [] : JSON.parse(storeString);
+        ACCEdata.push(data);
+        this.util.setCookie(this.store.storeCodeErr, JSON.stringify(ACCEdata));
+    },
+    setAjErrAc: function (url, xhr, textStatus) {
+        var storeString = this.util.getCookie(this.store.storeReqErr);
+        var ACEdata = this.util.isNullOrEmpty(storeString) ? [] : JSON.parse(storeString);
+        var nowStr = this.util.getTimeStr();
+        var ErrorData = {
+            type: this.store.storeReqErr,
+            path: this.util.getCookie(this.store.storePage),
+            sTme: nowStr,
+            requrl: url,
+            readyState: xhr.readyState,  //状态码
+            status: xhr.status,
+            statusText: xhr.statusText,
+            textStatus: textStatus
+        };
+        ACEdata.push(ErrorData);
+        this.util.setCookie(this.store.storeClick, JSON.stringify(ACEdata))
+    },
+    setInputAc: function (e) { //输入框操作数据保存
+        var storeString = this.util.getCookie(this.store.storeInput);
+        var elementId = e.id;
+        var className = e.className;
+        var storeKey = '#' + elementId + '|' + className; //存储主键，保证同一元素不重复添加
+        var ACIdata = this.util.isNullOrEmpty(storeString) ? {} : JSON.parse(storeString);
+        var inputData = ACIdata[storeKey];
+        //已存在的数据做补充不新增
+        var now = new Date().getTime();
+        var nowStr = this.util.getTimeStr();
+
+        if (this.util.isNullOrEmpty(inputData)) {
+            inputData = {};
+            inputData.type = this.store.storeInput;
+            inputData.path = this.util.getCookie(this.store.storePage);
+            inputData.eId = elementId;
+            inputData.class = className;
+            inputData.val = e.value || e.innerText;
+            inputData.sTme = nowStr;
+            inputData.eTme = nowStr
+        } else {
+            inputData.val += ("," + (now - new Date(inputData.eTme).getTime()) + ":" + e.value);
+            inputData.eTme = nowStr;
+        }
+        ACIdata[storeKey] = inputData;
+        this.util.setCookie(this.store.storeInput, JSON.stringify(ACIdata));
+    },
+    setClickAc: function (e) {  //元素点击数据保存
+        if (this.util.isNullOrEmpty(e.id) && this.util.isNullOrEmpty(e.className)) {
+            return;
+        }
+        if (['container'].indexOf(e.id) > -1 || e.innerText.length > 10) {
+            return;
+        }
+        var storeString = this.util.getCookie(this.store.storeClick);
+        var ACBdata = this.util.isNullOrEmpty(storeString) ? [] : JSON.parse(storeString);
+
+        var nowStr = this.util.getTimeStr();
+        var clickData = {
+            type: this.store.storeClick,
+            path: this.util.getCookie(this.store.storePage),
+            eId: e.id,
+            class: e.className,
+            val: e.value || e.innerText,
+            sTme: nowStr,
+            eTme: nowStr
+        };
+        ACBdata.push(clickData);
+        this.util.setCookie(this.store.storeClick, JSON.stringify(ACBdata))
+    },
+    getAc2Type: function (type) {  //获取本地数据
+        var storeArr = [];
+        var storeString = this.util.getCookie(type);
+        if (!this.util.isNullOrEmpty(storeString)) {
+            storeArr = JSON.parse(storeString);
+        }
+        this.util.delCookie(type);
+        return storeArr;
+    },
+    getACBtarget: function (node, length) {//冒泡场景下将除document外所有父元素添加点击事件
+        if (this.util.isNullOrEmpty(length)) {
+            length = 0;
+        }
+        //length限制采集内容大小，只采集有效数据
+        if (!this.util.isNullOrEmpty(node)) {
+            var parentNode = node && node.parentNode;
+            this.setClickAc(node);
+            if (Object.prototype.toString.call(parentNode) !== Object.prototype.toString.call(document) && length < this.store.acblength) {
+                this.getACBtarget(parentNode, ++length);
+            }
+        }
+    },
+    postData: function (data) {//数据上报
+        var uuid = this.util.getCookie(this.store.userSha) || this.util.getUuid;
+        var _this = this;
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            data: {uuid: uuid, acData: data},
+            url: _this.store.sendUrl
+        });
+    }
+};
+if (typeof define === "function" && define.amd) {
+    define("dataAc", ['jquery'], function () {
+        return dataAcquisition.init();
+    });
+} else {
+    dataAcquisition.init();
+}
